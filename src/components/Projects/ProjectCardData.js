73bcb5304075ef120a9830cc625856c182f43a2e
Nirmal Kumar Gupta <92977085+The-ZeGOD@users.noreply.github.com>
2024-08-15 01:40:30 +0530

    Update ProjectCardData.js

--- src/components/Projects/ProjectCardData.js
+++ src/components/Projects/ProjectCardData.js
@@ -1,29 +1,37 @@
 import pro1 from "../../assets/AmazonClone.jpg"
 import pro2 from "../../assets/NoteTaker.jpg"
 import pro3 from "../../assets/Calculator.jpg"
+import pro4 from "../../assets/Connectify.png"
 
 const ProjectData = [
-    {
-      imgsrc: pro1,
-      title: "Amazon Clone",
-      description: "The Amazon Clone project demonstrates the power of HTML and CSS in creating a responsive and visually appealing website. With its clean design and intuitive layout, it provides a solid foundation for future enhancements and customization.",
-      view: "https://amazon-clooneee.netlify.app/",
-      source: "https://github.com/The-ZeGOD/AmazonClone",
-    },
-    {
-      imgsrc: pro2,
-      title: "NoteTaker Website",
-      description: "The NoteTaker website offers users a seamless note-taking experience, allowing them to log in via email and efficiently create, update, and organize their notes. With robust session management in place, users can securely access their notes from any device, while the clean and modern design sets the stage for future enhancements and customization.",
-      view: "https://timely-tanuki-eef6f7.netlify.app/",
-      source: "https://github.com/The-ZeGOD/NoteTaker-BackEnd",
-    },
-    {
-      imgsrc: pro3,
-      title: "Calculator",
-      description: "The calculator website, crafted with HTML, CSS, and JavaScript, provides users with a versatile tool for quick and accurate calculations. Its intuitive interface facilitates effortless numerical input and calculation, enhancing user efficiency. With a sleek design and responsive layout, this website delivers a seamless user experience across various devices, making it a go-to resource for both simple and complex mathematical tasks.",
-      view: "https://the-zegod.github.io/MyCalculator/",
-      source: "https://github.com/The-ZeGOD/MyCalculator",
-    },
-]; 
+  {
+    imgsrc: pro4,
+    title: "Connectify",
+    description: "Connectify, built using the MERN stack (MongoDB, Express.js, React.js, Node.js), is a feature-rich chat application designed to facilitate real-time communication. It provides users with a seamless messaging experience, complete with robust features such as secure authentication, dynamic contact management, and responsive design. Connectify ensures efficient and secure communication, making it an ideal platform for both personal and professional interactions, accessible across multiple devices.",
+    view: "https://connectify-chat-app-ten.vercel.app",
+    source: "https://github.com/The-ZeGOD/Connectify-Chat-App",
+  },
+  {
+    imgsrc: pro1,
+    title: "Amazon Clone",
+    description: "The Amazon Clone project demonstrates the power of HTML and CSS in creating a responsive and visually appealing website. With its clean design and intuitive layout, it provides a solid foundation for future enhancements and customization.",
+    view: "https://amazon-clooneee.netlify.app/",
+    source: "https://github.com/The-ZeGOD/AmazonClone",
+  },
+  {
+    imgsrc: pro2,
+    title: "NoteTaker Website",
+    description: "The NoteTaker website offers users a seamless note-taking experience, allowing them to log in via email and efficiently create, update, and organize their notes. With robust session management in place, users can securely access their notes from any device, while the clean and modern design sets the stage for future enhancements and customization.",
+    view: "https://timely-tanuki-eef6f7.netlify.app/",
+    source: "https://github.com/The-ZeGOD/NoteTaker-BackEnd",
+  },
+  {
+    imgsrc: pro3,
+    title: "Calculator",
+    description: "The calculator website, crafted with HTML, CSS, and JavaScript, provides users with a versatile tool for quick and accurate calculations. Its intuitive interface facilitates effortless numerical input and calculation, enhancing user efficiency. With a sleek design and responsive layout, this website delivers a seamless user experience across various devices, making it a go-to resource for both simple and complex mathematical tasks.",
+    view: "https://the-zegod.github.io/MyCalculator/",
+    source: "https://github.com/The-ZeGOD/MyCalculator",
+  },
+];
 
 export default ProjectData;
